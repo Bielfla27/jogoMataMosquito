2caa17f717470233790430d2d9a376f00e3c6b6f
Bielfla27 <gabibielmft@outlook.com>
2021-08-18 10:27:14 -0300

    ajustando tamanho da tela do jogo

---
+++ js/jogo.js
@@ -0,0 +1,10 @@
+var altura =  0
+var largura = 0
+
+function ajustaTamanho(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(altura ,largura)
+}
+
+ajustaTamanho()
