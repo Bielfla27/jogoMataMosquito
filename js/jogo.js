5c000ed3fa46066d5eb4f5e1cdcf07848f855919
Bielfla27 <gabibielmft@outlook.com>
2021-08-18 11:15:38 -0300

    alterando tamanho do mosquito

--- js/jogo.js
+++ js/jogo.js
@@ -13,7 +13,7 @@ function posicaoRandomica(){
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
-    posicaoX = posicaoX < 0 ? 0 : posicaoX    
+    posicaoX = posicaoX < 0 ? 0 : posicaoX // se ele for menor que 0 ele rebecebe 0 se não ele mesmo
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(posicaoX,posicaoY)
@@ -22,9 +22,23 @@ function posicaoRandomica(){
 
     var mosquito = document.createElement('img')
     mosquito.src = "imagens/mosca.png"
-    mosquito.className = 'mosquito1'
+    mosquito.className =  tamanhoMosquito()
     mosquito.style.left = posicaoX + 'px' //coloando o mosquito nessa posicao
     mosquito.style.top = posicaoY + 'px' //colocando o mosquito nessa posicao
     mosquito.style.position = 'absolute' //mosquito tem que ser absolute
     document.body.appendChild(mosquito) //estou colocando o mosquito no body
+
+}
+
+function tamanhoMosquito(){
+    var classe =  Math.floor(Math.random() * 3)
+    
+   if(classe == 0){
+       return 'mosquito1'
+   }else if(classe == 1){
+       return 'mosquito2'
+   }else if(classe == 2){
+       return 'mosquito3'
+   }
+
 }
